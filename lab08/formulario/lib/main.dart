c8cca3d862c58efde22e278484b9363bf3b90d7f
Itsai <itsai@ensenada.gob.mx>
2022-03-12 16:33:15 -0800

    formulario primer parte terminada

--- lab08/formulario/lib/main.dart
+++ lab08/formulario/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:formulario/pages/index.dart';
 
-void main() => runApp(FormularioApp());
+void main() => runApp(const FormularioApp());
 
 class FormularioApp extends StatefulWidget {
   const FormularioApp({Key? key}) : super(key: key);
@@ -10,6 +11,20 @@ class FormularioApp extends StatefulWidget {
 }
 
 class _FormularioAppState extends State<FormularioApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Formulario', home: MyForm());
+  }
+}
+
+class MyForm extends StatefulWidget {
+  const MyForm({Key? key}) : super(key: key);
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
   String name = '';
   String email = '';
   DateTime selectedDate = DateTime.parse("1997-04-23");
@@ -30,156 +45,158 @@ class _FormularioAppState extends State<FormularioApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Formulario',
-        home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/fondo.jpg"),
-                fit: BoxFit.cover,
-              ),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/fondo.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, left: 10, right: 10),
+          child: ListView(scrollDirection: Axis.vertical, children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text('Create new\n  Account', style: TextStyle(fontSize: 40)),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Already Registered? Log in here',
+                    style: TextStyle(fontSize: 20))
+              ],
             ),
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 100, left: 10, right: 10),
-              child: ListView(scrollDirection: Axis.vertical, children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text('Create new\n  Account',
-                        style: TextStyle(fontSize: 40)),
-                    SizedBox(
+            const SizedBox(
+              height: 50,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // input nombre
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'NAME',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          style: const TextStyle(fontSize: 20),
+                          keyboardType: TextInputType.name,
+                          decoration: decorator(),
+                          onChanged: (String name) =>
+                              this.name = name.toString(),
+                        )
+                      ],
+                    ), //termina input nombre
+                    const SizedBox(
                       height: 20,
                     ),
-                    Text('Already Registered? Log in here',
-                        style: TextStyle(fontSize: 20))
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //input email
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // input nombre
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'NAME',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            TextField(
-                              style: const TextStyle(fontSize: 20),
-                              keyboardType: TextInputType.name,
-                              decoration: Decorator(),
-                              onChanged: (String name) => this.name = name,
-                            )
-                          ],
-                        ), //termina input nombre
-                        const SizedBox(
-                          height: 20,
+                        const Text(
+                          'EMAIL',
+                          style: TextStyle(fontSize: 20),
                         ),
-                        //input email
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'EMAIL',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            TextField(
-                              style: const TextStyle(fontSize: 20),
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: Decorator(),
-                              onChanged: (String email) => this.email = email,
-                            )
-                          ],
-                        ), // termina input email
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        //input password
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'PASSWORD',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            TextField(
-                                obscureText: true,
-                                style: const TextStyle(fontSize: 20),
-                                decoration: Decorator())
-                          ],
-                        ), //termina input password
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        //input date
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'DATE OF BIRTH',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            TextField(
-                              style: const TextStyle(fontSize: 20),
-                              keyboardType: TextInputType.datetime,
-                              decoration: InputDecoration(
-                                  hintText: 'Select',
-                                  focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(3.0),
-                                    borderSide: const BorderSide(
-                                        color: Colors.black, width: 2),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(3.0),
-                                    borderSide: const BorderSide(
-                                        color: Colors.black, width: 2),
-                                  ),
-                                  fillColor: Colors.white,
-                                  filled: true,
-                                  suffixIcon: TextButton(
-                                      onPressed: () => _selectDate(context),
-                                      child: const Icon(
-                                        Icons.calendar_today,
-                                        color: Colors.black,
-                                      ))),
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        ElevatedButton(
-                          style: TextButton.styleFrom(
-                              primary: Colors.white,
-                              textStyle: const TextStyle(fontSize: 20),
-                              backgroundColor: Colors.black,
-                              minimumSize: const Size(double.infinity, 60)),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SecondPage()));
-                          },
-                          child: const Text('Sing Up'),
+                        TextField(
+                          style: const TextStyle(fontSize: 20),
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: decorator(),
+                          onChanged: (String email) =>
+                              this.email = email.toString(),
                         )
-                        //termina input date
                       ],
-                    )),
-              ]),
-            ),
-          ),
-        ));
+                    ), // termina input email
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //input password
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'PASSWORD',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                            obscureText: true,
+                            style: const TextStyle(fontSize: 20),
+                            decoration: decorator())
+                      ],
+                    ), //termina input password
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //input date
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'DATE OF BIRTH',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          style: const TextStyle(fontSize: 20),
+                          keyboardType: TextInputType.datetime,
+                          decoration: InputDecoration(
+                              hintText: 'Select',
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(3.0),
+                                borderSide: const BorderSide(
+                                    color: Colors.black, width: 2),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(3.0),
+                                borderSide: const BorderSide(
+                                    color: Colors.black, width: 2),
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                              suffixIcon: TextButton(
+                                  onPressed: () {
+                                    _selectDate(context);
+                                  },
+                                  child: const Icon(
+                                    Icons.calendar_today,
+                                    color: Colors.black,
+                                  ))),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          textStyle: const TextStyle(fontSize: 20),
+                          backgroundColor: Colors.black,
+                          minimumSize: const Size(double.infinity, 60)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SecondPage(
+                                    name: this.name, email: this.email)));
+                      },
+                      child: const Text('Sing Up'),
+                    )
+                    //termina input date
+                  ],
+                )),
+          ]),
+        ),
+      ),
+    );
   }
 
-  InputDecoration Decorator() {
+  InputDecoration decorator() {
     return InputDecoration(
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(3.0),
@@ -191,21 +208,6 @@ class _FormularioAppState extends State<FormularioApp> {
       ),
       fillColor: Colors.white,
       filled: true,
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-      ),
-      body: Center(
-        child: Text('lkjlk'),
-      ),
     );
   }
 }
--- lab08/formulario/lib/pages/index.dart
+++ lab08/formulario/lib/pages/index.dart
@@ -1,1 +1,72 @@
+import 'package:flutter/material.dart';
 
+class SecondPage extends StatelessWidget {
+  final String name;
+  final String email;
+
+  const SecondPage({Key? key, required this.name, required this.email})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/fondo.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 200, left: 20),
+            child: RichText(
+              textAlign: TextAlign.justify,
+              text: TextSpan(
+                style: const TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.black,
+                ),
+                children: <TextSpan>[
+                  const TextSpan(text: 'Bienvenida/o '),
+                  TextSpan(
+                      text: '${capitalizeName()} \n',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const TextSpan(
+                      text:
+                          'enviaremos información a tu \ncorreo electrónico registrado\n'),
+                  TextSpan(
+                      text: email,
+                      style: const TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  String capitalizeName() {
+    String res = '';
+    for (var text in name.split(' ')) {
+      res += text.substring(0, 1).toUpperCase() + text.substring(1) + ' ';
+    }
+    return res;
+  }
+}
